5a52f9c8a7e8c7f6bb495feb51739a2e4489cf53
Vinci Wu <wu.vinci00@gmail.com>
2023-05-10 17:07:59 -0400

    Added a list to contain all elements and created an iterator to set each element properly in the grid

--- periodic/MainWindow.xaml.cs
+++ periodic/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,69 @@ namespace periodic
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Element> elements = new List<Element>
+        {
+            new Element {AtomicNumber = 1, Symbol = "H", Name = "Hydrogen", AtomicMass = 1.0080F, GroupBlock = "Nonmetal", Row = 0, Column = 0},
+            new Element {AtomicNumber = 2, Symbol = "He", Name = "Helium", AtomicMass = 4.00260F, GroupBlock = "Noble Gas", Row = 0, Column = 19}
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Add elements to their proper place
+            foreach(Element el in elements) {
+                // Create a Stack Panel 
+                StackPanel stackPanel = new StackPanel();
+                stackPanel.Orientation = Orientation.Vertical;
+
+                // Create a horizontal Stack Panel
+                StackPanel atomicData = new StackPanel();
+                atomicData.Orientation = Orientation.Horizontal;
+                atomicData.HorizontalAlignment = HorizontalAlignment.Stretch;
+
+                // Add Atomic Number to upper left
+                TextBlock atomicNum = new TextBlock();
+                atomicNum.Text = el.AtomicNumber.ToString();
+                atomicNum.HorizontalAlignment = HorizontalAlignment.Left;
+                atomicNum.FontSize = 12;
+                atomicData.Children.Add(atomicNum);
+
+                // Add Atomic Mass to upper right
+                TextBlock atomicMass = new TextBlock();
+                atomicMass.Text = el.AtomicMass.ToString();
+                atomicMass.HorizontalAlignment = HorizontalAlignment.Right;
+                atomicMass.FontSize = 12;
+                atomicData.Children.Add(atomicMass);
+
+                stackPanel.Children.Add(atomicData); 
+
+                // Add the element's symbol
+                TextBlock symbol = new TextBlock();
+                symbol.Text = el.Symbol;
+                symbol.TextAlignment = TextAlignment.Center;
+                symbol.FontSize = 20;
+                stackPanel.Children.Add(symbol);
+
+                // Add its Name under the symbol
+                TextBlock name = new TextBlock();
+                name.Text = el.Name;
+                name.TextAlignment = TextAlignment.Center;
+                name.FontSize = 12;
+                stackPanel.Children.Add(name);
+
+                // Wrap the StackPanel with a Border
+                Border border = new Border();
+                border.BorderThickness = new Thickness(1);
+                border.BorderBrush = Brushes.Black;
+                border.Child = stackPanel;
+                border.Margin = new Thickness(1);
+
+                // Add the Border to the grid
+                Grid.SetRow(border, el.Row);
+                Grid.SetColumn(border, el.Column);
+                PeriodicTable.Children.Add(border);
+            }
         }
     }
 }
