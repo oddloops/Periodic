31841a8ecba1868c5e7ec3deddca831d65a9f8a7
Vinci Wu <wu.vinci00@gmail.com>
2023-05-24 23:26:18 -0400

    Created skeleton for ElementPopupWindow to create a ui when an element is clicked

---
+++ periodic/ElementWindows/ElementPopupWindow.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ElementWindows
+{
+    public partial class ElementPopupWindow : Window
+    {
+        public ElementPopupWindow()
+        {
+            InitializeUI();
+        }
+
+        private void InitializeUI()
+        {
+            
+        }
+    }
+}
